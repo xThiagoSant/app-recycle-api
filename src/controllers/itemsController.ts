97c3da9ee26d0e003565b0e1f61e88f48d28c2f1
xthiagosant <uzmaki_thiago@outlook.com>
2020-06-05 00:19:42 -0300

    ajuste no controlador de itens

--- src/controllers/itemsController.ts
+++ src/controllers/itemsController.ts
@@ -5,10 +5,13 @@ class ItemsController {
     async index (req: Request, res: Response) {
         const items = await knex('items').select('*')
     
+        //coloquei na url da imagem o ip do expo para aula 4
+        // lembrar de colocar o url das imagens hospedadas
         const serializedItems = items.map(item =>{
             return {
+                id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`,
+                image_url: `http://192.168.1.15:3333/uploads/${item.image}`,
             }
         })
         return res.json(serializedItems)
